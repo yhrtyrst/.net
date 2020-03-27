f60123fd19022c95718c33b566dafc66602b3f00
BlairHsu <yhrtyrst@gmail.com>
2020-03-27 17:13:40 +0800

    first commit

---
+++ vidly-mvc-5-master/Vidly/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        
+        //data annotatiion
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        
+        public bool IsSubscribedToNewsletter { get; set; }
+        
+        public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        //Min18YearsIfAMember is custom add validation annotation
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
